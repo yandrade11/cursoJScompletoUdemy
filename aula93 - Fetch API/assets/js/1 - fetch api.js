0df8f35a4180feffe98e8ac84218a83dc43beae7
YURI ANDRADE <yuri.g.andrade@gmail.com>
2023-11-23 15:51:44 -0300

    feat: aula 93 fetch api

---
+++ aula93 - Fetch API/assets/js/1 - fetch api.js
@@ -0,0 +1,12 @@
+//FETCH API: já retorna por padrão uma PROMISE
+fetch('pagina3.html').then(response => {
+    //numa PROMISE sempre que eu "lanço (throw)" um erro no .then() ele direciona pro .catch()
+    if (response.status !== 200) throw new Error('ERRO 404 MEU');
+
+    return response.text(); //.text() retorna outra PROMISE, por tanto, precisa de outro .then()
+}).then(html => console.log(html))
+    .catch(erro => {
+        console.log('ERRO: ', erro);   //log: fundo branco
+        console.warn('ERRO: ', erro);   //warn: fundo amarelo
+        console.error('ERRO: ', erro);   //error: fundo vermelho
+    });
---
+++ aula93 - Fetch API/assets/js/2 - fetch na pratica.js
@@ -0,0 +1,44 @@
+//pegando click em toda pagina
+document.addEventListener('click', elem => {
+    const element = elem.target;
+    const tag = element.tagName.toLowerCase();
+
+    if (tag === 'a') {
+        //prevenindo o evento de click, ou seja, quando clicar não vai pra outra página
+        elem.preventDefault();
+        carregaPaginaAsync(element);
+    }
+});
+
+//COM PROMISE (trocar chamada acima, se quiser testar)
+function carregaPagina(element) {
+    const href = element.getAttribute('href');
+
+    fetch(href)
+        .then(response => {
+            if (response.status !== 200) throw new Error('ERRO tratado: ');
+            //fetch retorna JSON e tem que transformar em texto para html
+            return response.text()
+        })
+        .then(html => carregaResultado(html))   //carrega html
+        .catch(error => console.log('ERRO: ', error));
+}
+
+//COM ASYNC e AWAIT
+async function carregaPaginaAsync(element) {
+    try {
+        const href = element.getAttribute('href');
+        const response = await fetch(href);
+
+        if (response.status !== 200) throw new Error('ERRO tratado: ');
+        const html = await response.text();
+        carregaResultado(html);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML += response;
+}
