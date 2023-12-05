3d16e1a0c4ed4bb4217c66ba5e2e84f4f835954e
YURI ANDRADE <yuri.g.andrade@gmail.com>
2023-12-05 15:14:29 -0300

    aula 95

---
+++ aula95 - como instalar BabelJS/main.js
@@ -0,0 +1,6 @@
+const nome = "Yuri";
+const obj = { nome };
+const novoObj = { ...nome };
+
+//só para ver o babel convertendo o código atualizado para um script mais obsoleto
+console.log(novoObj);
