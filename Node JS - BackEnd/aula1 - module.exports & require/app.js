96f05a2384773e08c364a4a69d31cf94d6ed3334
YURI ANDRADE <yuri.g.andrade@gmail.com>
2023-12-11 15:20:15 -0300

    fix: mod1

--- Node JS - BackEnd/aula1 - module.exports & require/app.js
+++ Node JS - BackEnd/aula1 - module.exports & require/app.js
@@ -1,22 +1,25 @@
-//como importar em NodeJS
+//como importar todo arquivo em NodeJS
 const mod1 = require("./mod1");
 
-//para importar coisa específica
+//duas formas para importar coisa(s) específica(s)
+const funcaoFalaNome = mod1.falaNome;
 const mod11 = require("./mod1").falaNome;
-
-//ou
-const falaNome = mod1.falaNome;
 
 //retorna undefined se não tiver nenhum return na função
 //se for feito sem arrow function em alguns casos também retorna undefined (mesmo com return)
 console.log("\n-------------IMPORTS-------------\n");
-console.log("import: ", mod1);
+
+console.log("mod1: ", mod1);
 console.log("mod11: ", mod11);
-console.log("falaNome: ", falaNome);
+console.log("falaNome: ", funcaoFalaNome);
+
+
+console.log("\n----------com destructuring----------\n");
 
 //também da pra fazer com destructuring
-const { nome, sobrenome, falaNome } = require("./mod1");
+const { NOME, SOBRENOME, falaNome } = require("./mod1");
 
-console.log('nome: ', nome);
-console.log('sobrenome: ', sobrenome);
-// console.log('falaNome: ', falaNome); como ja foi declarado antes deixei comentado mas funciona apagando o de cima.
+//repare a forma que você exportou nome e sobrenome e não suas variáveis
+console.log("nome: ", NOME);
+console.log("sobrenome: ", SOBRENOME);
+console.log("falaNome: ", falaNome);
