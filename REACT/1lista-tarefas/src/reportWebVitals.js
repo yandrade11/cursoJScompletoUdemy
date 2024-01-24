e0d1dd9a34ffe317004c35316b932041f512bc54
YURI ANDRADE <yuri.g.andrade@gmail.com>
2024-01-24 17:02:15 -0300

    fix: aula 2

--- REACT/1lista-tarefas/src/reportWebVitals.js
+++ REACT/1lista-tarefas/src/reportWebVitals.js
@@ -1,13 +1,16 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
-};
 
-export default reportWebVitals;
+//SÓ DEIXEI AQUI PARA APRENDER SOBRE
+
+// const reportWebVitals = onPerfEntry => {
+//   if (onPerfEntry && onPerfEntry instanceof Function) {
+//     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+//       getCLS(onPerfEntry);
+//       getFID(onPerfEntry);
+//       getFCP(onPerfEntry);
+//       getLCP(onPerfEntry);
+//       getTTFB(onPerfEntry);
+//     });
+//   }
+// };
+
+// export default reportWebVitals;
