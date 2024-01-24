1c59e6e916262bb00aaa75050a80156dbd5bda64
YURI ANDRADE <yuri.g.andrade@gmail.com>
2024-01-24 17:00:25 -0300

    feat: aula 2

--- REACT/1lista-tarefas/src/App.js
+++ REACT/1lista-tarefas/src/App.js
@@ -1,26 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+//IMPORTS
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {/* Edit <code>src/App.js</code> and save to reload. */}
-          Hello, MotherFucker, world!!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//função que retorna JSX
+export default function App() {
+  return <h1>Hello world</h1>;
 }
 
-export default App;
+// function App() {
+//   return (
+//     //não é HTML, é JSX
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           Hello World!
+//         </p>
+//       </header>
+//     </div>
+//   );
+// }
+
+//EXPORT
+// export default App;
--- REACT/1lista-tarefas/src/index.js
+++ REACT/1lista-tarefas/src/index.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//ENTRY POINT (aqui starta o projeto)
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//IMPORTS
+//OBS: sempre que você for utilizar .jsx, você precisa importar o react, mesmo que não vá utilizar aqui
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+
+
+//antigamente (epoca da aula) você podia renderizar diretamente com ReactDOM.render passando classe/função APP e rota como argumento
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//hoje em dia, você cria uma variável root (que é a rota) e depois renderiza a variável passando classe/função APP e rota como argumento
 root.render(
   <React.StrictMode>
     <App />
@@ -14,4 +21,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
