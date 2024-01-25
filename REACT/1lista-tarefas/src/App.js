7ecf2b7639b0140394d3cf3f1a5def335d668aec
YURI ANDRADE <yuri.g.andrade@gmail.com>
2024-01-25 14:30:41 -0300

    fix aula 2 react

--- REACT/1lista-tarefas/src/App.js
+++ REACT/1lista-tarefas/src/App.js
@@ -1,10 +1,16 @@
 // IMPORTS
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 // função que retorna JSX
 export default function App() {
-  return <h1>Hello world</h1>;
+  //não é possível colocar mais de um elemento no return do JSX, mas você pode englobar tudo numa <div> ou <>.
+  return (
+    <>
+      <h1>Hello world</h1>
+      <p>Lorem impsum</p>
+    </>
+  );
 }
 
 // function App() {
