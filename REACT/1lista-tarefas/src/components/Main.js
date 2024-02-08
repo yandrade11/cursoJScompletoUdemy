d5cb0f9baec7d7d92ca7e5b5684b31b2448c34e8
YURI ANDRADE <yuri.g.andrade@gmail.com>
2024-02-08 17:01:03 -0300

    feat: aula 197 e 198

--- REACT/1lista-tarefas/src/components/Main.js
+++ REACT/1lista-tarefas/src/components/Main.js
@@ -17,7 +17,7 @@ export default class Main extends Component {
   state = {
     //inicializando um STATE
     novaTarefa: '',
-    tarefas: ['Fazer café', 'Beber água', 'Estudar'],
+    tarefas: [],
   };
 
   //captura valor digitado no input e salva temporariamente no state
@@ -30,18 +30,57 @@ export default class Main extends Component {
     });
   };
 
+  handleSubmit = (event) => {
+    //previne envio do form (atualizar a página)
+    event.preventDefault();
+
+    const { tarefas } = this.state;
+    let { novaTarefa } = this.state;
+    novaTarefa = novaTarefa.trim();
+
+    //se o index de novaTarefa estiver dentro do array tarefas
+    // if (tarefas.indexOf(novaTarefa) !== -1) return;
+
+    //como eu não posso editar o estado tarefas diretamente: clono ela para uma nova variável
+    const clonaTarefas = [...tarefas, novaTarefa];
+
+    //depois de clonar tarefas, eu jogo
+    this.setState({
+      //SPREAD OPERATOR: desestrutura array e copia exatamente igual
+      tarefas: [...clonaTarefas],
+    });
+
+    //limpa input
+    const inputText = document.querySelector('input');
+    inputText.value = '';
+  };
+
+  handleEdit = (event, index) => {
+    console.log('edit', index);
+  };
+
+  handleDelete = (event, index) => {
+    const { tarefas } = this.state;
+    const clonaTarefas = [...tarefas];
+    clonaTarefas.splice(index, 1);
+
+    this.setState({
+      tarefas: [...clonaTarefas],
+    });
+  };
+
   //componente STATEFULL são geralmente classes e precisam do render(para renderizar algo na tela)
   render() {
     // importando valor da propriedade (novaTarefa) do STATE
-    // const novaTarefa = this.state.novaTarefa; OU
-    const { novaTarefa, tarefas } = this.state;
+    // const novaTarefa = this.state.novaTarefa; (se tiver só 1) OU
+    const { /* novaTarefa, */ tarefas } = this.state;
 
     return (
       //no JSX usamos className pq class é uma palavra reservada do JS
       <div className="main">
         <h1>Lista de tarefas</h1>
 
-        <form action="#" className="form">
+        <form onSubmit={this.handleSubmit} action="#" className="form">
           {/* para usar JS no JSX usar "{}" */}
           {/* onChange: ao mudar algo no input, chama função handleChange */}
           <input
@@ -49,9 +88,9 @@ export default class Main extends Component {
             placeholder="Digite aqui sua tarefa"
             type="text"
             // repare que no value estou chamando o STATE sem aspas
-            value={novaTarefa}
+            // value={novaTarefa} //comentado limpar input após o clique do botão
           />
-          <button type="submit">
+          <button type="submit" tabIndex={0}>
             {/* chamando FaPlus que foi importado do react-icons*/}
             <FaPlus />
           </button>
@@ -61,14 +100,21 @@ export default class Main extends Component {
         {/* <h2>{tarefas}</h2> */}
 
         <ul className="tarefas">
-          {tarefas.map((tarefa) => (
+          {tarefas.map((tarefa, index) => (
             //KEY: serve como uma INDEX para o REACT, nesse caso ele usa a própria tarefa (e depois vai tratar pra não ter tarefa igual)
-            <li key={tarefa}>
-              {tarefa}
-              <div>
-                <FaEdit className='edit' />
-                <FaWindowClose className='delete' />
-              </div>
+            <li key={index}>
+              {`${index + 1} - ${tarefa}`}
+              <span>
+                <FaEdit
+                  //como os métodos tem 2 params, tem que escrever assim o invés de onClick={this.handleEdit}
+                  onClick={(event) => this.handleEdit(event, index)}
+                  className="edit"
+                />
+                <FaWindowClose
+                  onClick={(event) => this.handleDelete(event, index)}
+                  className="delete"
+                />
+              </span>
             </li>
           ))}
         </ul>
